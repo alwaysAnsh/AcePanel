47ede2ce68b16b851ccb561de55475ab2e5f414e
alwaysAnsh <anshj9y@gamil.com>
2025-06-26 00:06:04 +0530

    implementation: create user webhook

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://modest-sole-96.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 // import "./globals.css"; 
 import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClerkProvider from "@/components/providers/ConvexClerkProvider";
 
 const geist = Geist({ subsets: ["latin"] });
 
@@ -11,12 +12,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ConvexClerkProvider>
     <html lang="en">
       <body className="" >
         {children}
       </body>
     </html>
-    </ClerkProvider>
+    </ConvexClerkProvider>
   );
 }
