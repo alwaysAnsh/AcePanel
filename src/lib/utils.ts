619d4f658e87822c5742acbeaa207ff6ffdc4a4e
alwaysAnsh <anshj9y@gamil.com>
2025-07-02 22:13:55 +0530

    implemented utility function for meeting card component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
 import { clsx, type ClassValue } from "clsx"
+import { addHours, isBefore, isWithinInterval } from "date-fns";
 import { twMerge } from "tailwind-merge"
+import { Doc } from "../../convex/_generated/dataModel";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+type Interview = Doc<"interviews">;
+type User = Doc<"users">;
+
+export const getMeetingStatus = (interview: Interview) => {
+  const now = new Date();
+  const interviewStartTime = interview.startTime;
+  const endTime = addHours(interviewStartTime, 1);
+
+  if (
+    interview.status === "completed" ||
+    interview.status === "failed" ||
+    interview.status === "succeeded"
+  )
+    return "completed";
+  if (isWithinInterval(now, { start: interviewStartTime, end: endTime })) return "live";
+  if (isBefore(now, interviewStartTime)) return "upcoming";
+  return "completed";
+};
